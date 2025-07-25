6092e3e1b93bab886e43e840a3db4a07de6c2467
Juggernaut7 <abdulkabir0600@gmail.com>
2025-07-25 22:17:42 +0100

    deployment issue resolved

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; // Your main Tailwind CSS file
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './components/Auth/AuthProvider';  
+// import { AuthProvider } from './components/Auth/AuthProvider';  
 import { ThemeProvider } from './hooks/useTheme'; // We'll create this soon
+import { AuthProvider } from './components/Auth/AuthProvider.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
